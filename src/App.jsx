74ecfa158638188cfb99591271dd068eb43d3dd9
sanjay <22bd1a056l@gmail.com>
2024-12-03 21:17:17 +0530

    inline css in React

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+const styling = {
+  display:"flex",
+  justifyContent:"center",
+  border:"10px solid black"
+
+}
+function App() {
+  return (
+    <h1 style={styling}>HEYYYY</h1>
+  )
+}
+export default App
